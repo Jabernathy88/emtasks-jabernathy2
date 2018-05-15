3ff281672a0dc55da6f6776867892f4c36bd68e3
Jabernathy88 <j.e.abernathy@gmail.com>
2018-05-15 08:55:42 -0400

    Added image asset and updated ember-moment helper.

---
+++ app/helpers/format-date.js
@@ -0,0 +1,8 @@
+// import Helper from '@ember/component/helper';
+// import moment from 'moment';
+
+// export function formatDate(params){
+//   return moment(params[0]).format('YYYY-MM-DD');
+// }
+
+// export default Helper.extend(formatDate);
