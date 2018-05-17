1c92c890d388230883e993c4f6fd2143a1c08c96
Jabernathy88 <j.e.abernathy@gmail.com>
2018-05-17 00:45:56 -0400

    Demo ready for full SPA prototype.

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -2,12 +2,5 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 	actions:{
-		deleteTask: function(id){
-			this.store.findRecord('task', id).then(function(task){
-				task.deleteRecord();
-
-				task.save();
-			});
-		}
 	}
 });
--- app/controllers/tasks/index.js
+++ app/controllers/tasks/index.js
@@ -2,7 +2,54 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 	actions:{
-		deleteTask: function(id){
+    addTask: function(){
+      let title = this.get('title');
+      let description = this.get('description');
+      let date = this.get('date');
+
+			// create New Task
+			let newTask = this.store.createRecord('task', {
+				title: title,
+				description: description,
+				date: new Date(date)
+      });
+
+			// save to Database
+			newTask.save(); 
+
+			// clear forms
+			this.setProperties({
+				title: '',
+				description: '',
+				date: ''
+			});
+    },
+    editTask: function(id){
+			let self = this;
+
+			let title = this.get('title');
+			let description = this.get('description');
+			let date = this.get('date');
+      // edit Task
+      
+      function failure(reason) {
+        // handle the error
+      }
+
+			this.store.findRecord('task', id).then(function(task){
+				task.set('title', title);
+				task.set('description', description);
+				task.set('date', new Date(date));
+
+        function transitionToIndex(post) {
+          self.transitionToRoute('tasks.index');
+        }
+
+				// Save to Database
+        task.save().then(transitionToIndex).catch(failure);
+      });
+    },
+    deleteTask: function(id){
 			this.store.findRecord('task', id).then(function(task){
 				task.deleteRecord();
 
